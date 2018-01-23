04152a5baca6336e1a9373f699104591751aa8e1
Viviana Brolite <supviviana@gmail.com>
2018-01-23 11:16:10 +0200

    mssg model

---
+++ app/Message.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'session_id', 'admin', 'message',
+    ];
+
+    protected $table = 'messages';
+    
+}
